9b8f327fdd2f36efaa32d965d6066626f0df40fd
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-29 18:39:49 +0100

    Configured redux in App.js

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {enableScreens} from "react-native-screens";
+import {createStore, combineReducers} from "redux";
+import {Provider} from "react-redux";
+import {composeWithDevTools} from "redux-devtools-extension/index";
+import productsReducer from "./redux/reducers/products.reducers";
+
+enableScreens();
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </Provider>
   );
 }
 

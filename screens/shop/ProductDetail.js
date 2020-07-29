187f0baba875150aba599f863aec5c9684dcf8c6
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-29 19:44:27 +0100

    Created basic ProductDetailScreen component

---
+++ screens/shop/ProductDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button, Image, ScrollView} from 'react-native';
+
+const ProductDetailScreen = props => {
+  return (
+    <View>
+      <Text>Product Detail Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
+
+export default ProductDetailScreen;

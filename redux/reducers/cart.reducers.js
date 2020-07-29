9e57032db54871aa8f5ece75718f688ca08d33cd
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-29 20:56:08 +0100

    Added cart reducer functionality to update or add cart item

--- redux/reducers/cart.reducers.js
+++ redux/reducers/cart.reducers.js
@@ -1,4 +1,5 @@
-import PRODUCTS from "../../data/dummy-data";
+import {ADD_TO_CART} from "../constants/cart.constants";
+import CartItem from "../../models/CartItem";
 
 const initialState = {
   items: {},
@@ -7,6 +8,30 @@ const initialState = {
 
 const cartReducer = (state=initialState, action) => {
   switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.payload;
+      const prodPrice = addedProduct.price;
+      const prodTitle = addedProduct.title;
+      let updatedOrNewCartItem;
+      if (state.items[addedProduct.id]) {
+        // Already have item in the cart - increment quantity and add to sum
+        const cartItem = state.items[addedProduct.id];
+        updatedOrNewCartItem = new CartItem(
+          cartItem.quantity + 1,
+          prodPrice,
+          prodTitle,
+          cartItem.sum + prodPrice
+        );
+      } else {
+        // Add new item to cart
+        updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+      return {
+        ...state,
+        items: {...state.items, [addedProduct.id]: updatedOrNewCartItem},
+        totalAmount: state.totalAmount + prodPrice,
+      };
+
     default:
       return state;
   }

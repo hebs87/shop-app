19ceb9ea1b41a7aa821e306641738d12bb058d9c
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-29 20:20:25 +0100

    Added fetch font functionality in App.js

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {enableScreens} from "react-native-screens";
 import {createStore, combineReducers} from "redux";
 import {Provider} from "react-redux";
+import {AppLoading} from "expo";
+import * as Font from 'expo-font';
 import {composeWithDevTools} from "redux-devtools-extension/index";
 import productsReducer from "./redux/reducers/products.reducers";
 import ShopNavigator from "./navigation/ShopNavigator";
@@ -14,7 +16,26 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer, composeWithDevTools());
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    OpenSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+    OpenSansBold: require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+}
+
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={setLoading(false)}
+        onError={error => console.log(error)}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator/>

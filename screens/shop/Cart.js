0027f0bbce78fbe5e842a5f67100870a7fe68ef7
sunny <sunny@jbcole.co.uk>
2020-09-19 22:52:59 +0100

    Created template CartScreen component

---
+++ screens/shop/Cart.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {StyleSheet, View, Text, FlatList, Button} from "react-native";
+import {useSelector} from "react-redux";
+import Colors from "../../theme/constants";
+
+const CartScreen = props => {
+  const cartTotalAmount = useSelector(state => state.cart.totalAmount);
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.summary}>
+        <Text style={styles.summaryText}>
+          Total: <Text style={styles.amount}>£{cartTotalAmount}</Text>
+        </Text>
+        <Button
+          title="Order Now"
+        />
+      </View>
+      <View>
+        <Text>CART ITEMS</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    margin: 20,
+  },
+  summary: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    padding: 10,
+    shadowColor: Colors.black,
+    shadowOpacity: 0.26,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: Colors.white,
+  },
+  summaryText: {
+    fontFamily: 'OpenSansBold',
+    fontSize: 18,
+  },
+  amount: {
+    color: Colors.accent,
+  },
+})
+
+export default CartScreen;

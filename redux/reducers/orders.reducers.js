a5218efa7c08e6f1dc2631c9ab3f72ff6782b50c
sunny <sunny@jbcole.co.uk>
2020-09-20 00:55:17 +0100

    Created reducer functionality to add order

--- redux/reducers/orders.reducers.js
+++ redux/reducers/orders.reducers.js
@@ -1,9 +1,25 @@
+import {ADD_ORDER} from "../constants/orders.constants";
+import Order from "../../models/Order";
+
 const initialState = {
   orders: [],
 }
 
 const ordersReducer = (state=initialState, action) => {
   switch (action.type) {
+    case ADD_ORDER:
+      const items = action.payload.items;
+      const amount = action.payload.amount;
+      const newOrder = new Order(
+        new Date().toString(),
+        items,
+        amount,
+        new Date()
+      );
+      return {
+        ...state,
+        orders: state.orders.concat(newOrder)
+      };
     default:
       return state;
   }

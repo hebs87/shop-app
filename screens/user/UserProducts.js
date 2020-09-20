592d146fb91049e648841a4a3886e813dd73a53a
sunny <sunny@jbcole.co.uk>
2020-09-20 22:06:24 +0100

    Created UserProductsScreen component

---
+++ screens/user/UserProducts.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {FlatList} from "react-native";
+import {useSelector} from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
+
+const UserProductsScreen = props => {
+  const products = useSelector(state => state.products.userProducts)
+
+  return (
+    <FlatList
+      keyExtractor={item => item.id}
+      data={products}
+      renderItem={({item}) => (
+        <ProductItem
+          image={item.imageUrl}
+          title={item.title}
+          price={item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
+  );
+};
+
+export default UserProductsScreen;

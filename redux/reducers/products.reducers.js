b601dcde4b2c990c823dd23e2837c61010e18e61
sunny <sunny@jbcole.co.uk>
2020-09-20 22:46:16 +0100

    Added product reducer case for deleting product

--- redux/reducers/products.reducers.js
+++ redux/reducers/products.reducers.js
@@ -1,4 +1,5 @@
 import PRODUCTS from "../../data/dummy-data";
+import {DELETE_PRODUCT} from "../constants/products.constants";
 
 const initialState = {
   availableProducts: PRODUCTS,
@@ -7,6 +8,17 @@ const initialState = {
 
 const productsReducer = (state=initialState, action) => {
   switch (action.type) {
+    case DELETE_PRODUCT:
+      const productId = action.payload;
+      return {
+        ...state,
+        availableProducts: state.availableProducts.filter(
+          product => product.id !== productId
+        ),
+        userProducts: state.userProducts.filter(
+          product => product.id !== productId
+        ),
+      }
     default:
       return state;
   }

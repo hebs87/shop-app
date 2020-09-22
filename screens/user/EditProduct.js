49e3f5dc1f61f9fc850b69a8d868ab7c63715421
sunny <sunny@jbcole.co.uk>
2020-09-22 12:11:03 +0100

    Created basic EditProductScreen component

---
+++ screens/user/EditProduct.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {StyleSheet, View, Text} from "react-native";
+
+const EditProductScreen = props => {
+  return (
+    <View>
+      <Text>Edit Product Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default EditProductScreen;

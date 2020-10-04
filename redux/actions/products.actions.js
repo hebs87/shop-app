ae8f676d5b08bd76540adaae39fd3fd677ed1840
sunny <sunny@jbcole.co.uk>
2020-10-04 15:01:39 +0100

    Created redux logic to update or create new product based on form input

--- redux/actions/products.actions.js
+++ redux/actions/products.actions.js
@@ -1,4 +1,18 @@
-import {DELETE_PRODUCT} from "../constants/products.constants";
+import {
+  CREATE_PRODUCT,
+  UPDATE_PRODUCT,
+  DELETE_PRODUCT,
+} from "../constants/products.constants";
+
+export const createProduct = data => ({
+  type: CREATE_PRODUCT,
+  payload: data
+});
+
+export const updateProduct = data => ({
+  type: UPDATE_PRODUCT,
+  payload: data
+});
 
 export const deleteProduct = productId => ({
   type: DELETE_PRODUCT,
--- redux/reducers/products.reducers.js
+++ redux/reducers/products.reducers.js
@@ -1,5 +1,10 @@
 import PRODUCTS from "../../data/dummy-data";
-import {DELETE_PRODUCT} from "../constants/products.constants";
+import {
+  CREATE_PRODUCT,
+  UPDATE_PRODUCT,
+  DELETE_PRODUCT,
+} from "../constants/products.constants";
+import Product from "../../models/Product";
 
 const initialState = {
   availableProducts: PRODUCTS,
@@ -8,6 +13,45 @@ const initialState = {
 
 const productsReducer = (state=initialState, action) => {
   switch (action.type) {
+    case CREATE_PRODUCT:
+      const data = action.payload;
+      const newProduct = new Product(
+        new Date().toString(),
+        'u1',
+        data.title,
+        data.imageUrl,
+        data.description,
+        data.price
+      );
+      return {
+        ...state,
+        availableProducts: state.availableProducts.concat(newProduct),
+        userProducts: state.userProducts.concat(newProduct)
+      }
+    case UPDATE_PRODUCT:
+      const productData = action.payload;
+      console.log(productData)
+      // Get the product index in userProducts, create an updated product and update the userProducts array
+      const productIndex = state.userProducts.findIndex(prod => prod.id === productData.id);
+      const updatedProduct = new Product(
+        productData.id,
+        state.userProducts[productIndex].ownerId,
+        productData.title,
+        productData.imageUrl,
+        productData.description,
+        state.userProducts[productIndex].price
+      );
+      const updatedUserProducts = [...state.userProducts];
+      updatedUserProducts[productIndex] = updatedProduct;
+      // Update the availableProducts array
+      const availableProductIndex = state.availableProducts.findIndex(prod => prod.id === productData.id);
+      const updatedAvailableProducts = [...state.availableProducts];
+      updatedAvailableProducts[availableProductIndex] = updatedProduct;
+      return {
+        ...state,
+        availableProducts: updatedAvailableProducts,
+        userProducts: updatedUserProducts
+      }
     case DELETE_PRODUCT:
       const productId = action.payload;
       return {

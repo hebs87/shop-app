cf4165172ba3fa23a925b8f6e12637ad05719c46
sunny <sunny@jbcole.co.uk>
2020-10-04 13:40:04 +0100

    Added form fields and styles to EditProduct screen

--- screens/user/EditProduct.js
+++ screens/user/EditProduct.js
@@ -1,16 +1,49 @@
 import React from "react";
-import {StyleSheet, View, Text} from "react-native";
+import {StyleSheet, ScrollView, View, TextInput, Text} from "react-native";
+import Colors from '../../theme/constants';
 
 const EditProductScreen = props => {
   return (
-    <View>
-      <Text>Edit Product Screen!</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput style={styles.input}/>
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Image URL</Text>
+          <TextInput style={styles.input}/>
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Price</Text>
+          <TextInput style={styles.input}/>
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput style={styles.input}/>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-
+  form: {
+   margin: 20,
+  },
+  formControl: {
+    width: '100%',
+  },
+  label: {
+    fontFamily: 'OpenSansBold',
+    marginVertical: 8,
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: Colors.lightGrey,
+    borderBottomWidth: 1,
+  },
 });
 
 export default EditProductScreen;
--- theme/constants.js
+++ theme/constants.js
@@ -4,5 +4,6 @@ export default {
   white: '#FFF',
   black: '#000',
   darkGrey: '#888',
+  lightGrey: '#CCC',
   opaque: 'rgba(0,0,0,0.5)',
 }
